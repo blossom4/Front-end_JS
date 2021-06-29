d93c04823d872ebe999379b8303b935af176043e
Ryan <cho8213456@gmail.com>
2021-06-29 14:24:46 +0900

    vuejs practice my first vue app

---
+++ Part50_Vuejs_Practice/01_vue_cli/새 폴더/src/components/NewComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>New Component!</h1>
+    <h1>{{ myMessage }}</h1>
+    <input @keyup.enter="childInputChange" v-model="childInputData" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComponent',
+  data: function () {
+    return {
+      childInputData: '',      
+    }
+  },
+  props: {
+    myMessage: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    childInputChange: function () {
+      this.$emit('child-emit-data', this.childInputData)
+      this.childInputData = ''
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ Part50_Vuejs_Practice/01_vue_cli/새 폴더/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <NewComponent @child-emit-data="parentGetData" my-message="Message from about page"/>
+  </div>
+</template>
+
+<script>
+import NewComponent from '@/components/NewComponent'
+
+export default {
+  name: 'About',
+  components: {
+    NewComponent,
+  },
+  methods: {
+    parentGetData: function (emitInfo) {
+      console.log(`${emitInfo}`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
