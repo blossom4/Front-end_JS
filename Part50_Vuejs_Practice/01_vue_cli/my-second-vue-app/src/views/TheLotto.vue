8e61aa9e6aa185239f0d02b01471dd0cd710b30c
Ryan <cho8213456@gmail.com>
2021-06-30 17:19:10 +0900

    vuejs practice 02

---
+++ Part50_Vuejs_Practice/01_vue_cli/my-second-vue-app/src/views/TheLotto.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>로또번호 추천</h1>
+    <button @click="getLottoNums">Click</button>
+    <h2>로또번호: {{ selectedLottoNums }}</h2>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'TheLotto',
+  data: function () {
+    return {
+      sampleNums: [],
+      selectedLottoNums: [],
+      i: Number
+    }
+  },
+  methods: {
+    getLottoNums: function () {
+      const numbers = _.range(1, 46)
+      this.selectedLottoNums = ''
+      this.sampleNums = _.sortBy(_.sampleSize(numbers, 6))
+      this.selectedLottoNums = this.sampleNums.join(' ')
+      // for (this.i = 0; this.i < 6; this.i +=1) {
+      //   this.selectedLottoNums += this.sampleNums[this.i] + ' '
+      // }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Part50_Vuejs_Practice/01_vue_cli/my-second-vue-app/src/views/TheLunch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>점심메뉴 추천</h1>
+    <button @click="pickOneLunchMenu">Click</button>
+    <p>오늘의 점심메뉴: {{ selectedMenu }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'TheLunch',
+  data: function () {
+    return {
+      lunch: ['간장계란밥🥣', '연어초밥🍣', '닭가슴살샐러드🥗'],
+      selectedMenu: '',
+    }
+  },
+  methods: {
+    pickOneLunchMenu: function () {
+      this.selectedMenu = _.sample(this.lunch)
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
