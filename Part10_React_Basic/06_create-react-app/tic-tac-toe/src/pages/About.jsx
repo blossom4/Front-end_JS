063c5ce51c74dadb5a66f31d8108034958e2ca64
Ryan <cho8213456@gmail.com>
2021-07-05 01:20:03 +0900

    react dynamic routing

--- Part10_React_Basic/06_create-react-app/tic-tac-toe/src/pages/About.jsx
+++ Part10_React_Basic/06_create-react-app/tic-tac-toe/src/pages/About.jsx
@@ -1,3 +1,32 @@
-export default function About() {
-  return <div>About</div>
+export default function About(props) {
+  console.log(props)
+  const searchParams = props.location.search
+  console.log(searchParams)
+  const obj = new URLSearchParams(searchParams)
+  console.log(obj.get('name'))
+  return (
+    <div>
+      <h2>About</h2>
+      { obj.get('name') && <p>name: { obj.get('name') }</p> }
+    </div>
+  )
 }
+
+// URLSearchParams를 지원하지 않는 브라우저와 호환하거나 key-value쌍으로 사용하기 원한다면
+// npm i query-string해서 import하여 사용한다.
+
+// import queryString from 'query-string'
+
+// export default function About(props) {
+//   console.log(props)
+//   const searchParams = props.location.search
+//   console.log(searchParams)
+//   const query = queryString.parse(searchParams)
+//   console.log(query)
+//   return (
+//     <div>
+//       <h2>About</h2>
+//       { query.name && <p>name: { query.name }</p> }
+//     </div>
+//   )
+// }
--- Part10_React_Basic/06_create-react-app/tic-tac-toe/src/pages/Profile.jsx
+++ Part10_React_Basic/06_create-react-app/tic-tac-toe/src/pages/Profile.jsx
@@ -1,3 +1,11 @@
-export default function Profile() {
-  return <div>Profile</div>
+export default function Profile(props) {
+  console.log(props)
+  const id = props.match.params.id
+  console.log(id, typeof id)
+  return (
+    <div>
+      <h2>Profile</h2>
+      {id && <p>id: {id}</p>}
+    </div>
+  )
 }
